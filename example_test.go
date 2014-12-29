391d6b8390f4ce3842d2318537ca86ebdafce66b
ggaaooppeenngg <471160171@qq.com>
2014-12-29 21:54:42 +0800

    add example

---
+++ example_test.go
@@ -0,0 +1,25 @@
+package cpic
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func ExampleGen() {
+	sr := strings.NewReader(`tree:
+	->black
+		->red
+		->red
+			->black`)
+	bs, e := ioutil.ReadAll(sr)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Printf("%s\n", bs)
+	out, err := Gen(string(bs))
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(out)
+}
